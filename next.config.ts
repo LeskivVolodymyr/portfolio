cf86c8756d6a8e46241561f52fb3eda974543b13
Volodymyr.Leskiv <leskiv.v.d@gmail.com>
2025-01-29 15:33:04 +0200

    dev configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
 import type { NextConfig } from 'next';
+import type { Configuration } from 'webpack';
 
-const nextConfig: NextConfig = {
-    /* config options here */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (phase: string): NextConfig => {
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+
+    return {
+        assetPrefix: isDev ? undefined : '/',
+        webpack: (config: Configuration) => {
+            if (isDev) {
+                config.watchOptions = {
+                    poll: 1000,
+                    aggregateTimeout: 300,
+                };
+            }
+
+            return config;
+        },
+    };
 };
-
-export default nextConfig;
