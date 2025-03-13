9d004a9103dc0c064933e4f8b61cbc4f460d74bc
Volodymyr.Leskiv <leskiv.v.d@gmail.com>
2025-03-14 00:13:59 +0200

    empty section

---
+++ src/app/_components/sections/MyExperienceSection/MyExperienceSection.tsx
@@ -0,0 +1,26 @@
+export default function MyExperienceSection() {
+    return (
+        <div className='flex flex-col flex-wrap  md:flex-row gap-2'>
+            <div className='flex-1'>
+                <h2 className='text-6xl'>MY EXPERIENCE</h2>
+            </div>
+            <div className='flex flex-col flex-1'>
+                <div>
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </div>
+                <div>
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </div>
+                <div>
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </div>
+                <div>
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </div>
+                <div>
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import LetsConnectSection from '@/app/_components/sections/LetsConnectSection/Le
 import HomeSection from '@/app/_components/sections/HomeSection/HomeSection';
 import SkillSection from '@/app/_components/sections/SkillSection/SkillSection';
 import SectionFullWidth from '@/app/_components/SectionFullWidth/SectionFullWidth';
+import MyExperienceSection from '@/app/_components/sections/MyExperienceSection/MyExperienceSection';
 
 export default function Page() {
     return (
@@ -13,6 +14,9 @@ export default function Page() {
             <SectionFullWidth id='skills'>
                 <SkillSection />
             </SectionFullWidth>
+            <Section id='my-experience'>
+                <MyExperienceSection />
+            </Section>
             <Section id='lets-connect' bottomSeparator={false}>
                 <LetsConnectSection />
             </Section>
