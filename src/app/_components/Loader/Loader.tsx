d0f3db17d47a6792388c62a187fe948f8c03d04e
Volodymyr.Leskiv <volodymyr.leskiv@intoglobal.com>
2026-02-16 03:00:14 +0200

    loader bg fix

--- src/app/_components/Loader/Loader.tsx
+++ src/app/_components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import styles from './Loader.module.scss';
 
 export default function Loader() {
     return (
-        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-20 z-50'>
+        <div className='fixed inset-0 flex items-center justify-center bg-black/20 z-50'>
             <div className={styles['lds-spinner']}>
                 {Array(12)
                     .fill(null)
